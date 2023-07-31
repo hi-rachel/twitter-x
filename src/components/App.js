aba477ca459d8cca907345a9db4665f5511a5e15
hi-rachel <rachel.uiux@gmail.com>
2023-07-31 14:59:28 +0900

    Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "routes/fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  });
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} TwitterX</footer>
+    </>
+  );
 }
 
 export default App;
