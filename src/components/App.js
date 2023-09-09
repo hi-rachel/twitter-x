bb094d400b3cadd2238eb103a1d153526afab55e
hi-rachel <rachel.uiux@gmail.com>
2023-09-10 00:42:13 +0900

    refactor: Delete and Update Tweet

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
-import { authService } from "routes/fbase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   const [userObj, setUserObj] = useState(null);
   const auth = getAuth();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -22,7 +17,7 @@ function App() {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         "Initializing..."
       )}
